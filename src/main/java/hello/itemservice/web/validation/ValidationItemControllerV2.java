0fb71a730baf5f492a77ad54d1bfbc371ff653b7
0540 <0540@coreit.co.kr>
2024-01-29 18:29:32 +0900

    bindingResult1

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -7,6 +7,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -43,37 +46,36 @@ public class ValidationItemControllerV2 {
     }
 
     @PostMapping("/add")
-    public String addItem(@ModelAttribute Item item,
+    public String addItemV1(@ModelAttribute Item item,
+                          BindingResult bindingResult, //순서가 중요 얘는 @ModelAttribute 다음에 와야함! 그래야 item의 에러를 담을 수 있음
                           RedirectAttributes redirectAttributes,
                           Model model) {
 
-        //검증 오류 결과를 보관
-        Map<String,String> errors = new HashMap<>();
-
-
         //검증 로직
         if(!StringUtils.hasText(item.getItemName())){ //item.ItemName에 글자가 없으면
-            errors.put("itemName", "상품이름은 필수입니다.");
+            //오브젝트명(@ModelAttribute에 담기는 이름을 넣어주면 됨, 필드명, 디폴트 메세지.
+            bindingResult.addError(new FieldError("item","itemName", "상품 이름은 필수입니다."));
         }
         if(item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000){ //가격이 없거나 천원보다 작거나 백만원보다 크면
-            errors.put("price", "가격은 1,000 ~ 1,000,000 까지 허용합니다.");
+            bindingResult.addError(new FieldError("item", "price", "가격은 1,000 ~ 1,000,000까지 허용합니다."));
         }
         if(item.getQuantity() == null || item.getQuantity() >= 9999){
-            errors.put("quantity", "수량은 최대 9,999 까지 허용합니다.");
+            bindingResult.addError(new FieldError("item", "quantity", "수량은 최대 9,999 까지 허용합니다."));
         }
 
         //특정 필드가 아닌 복합 룰 검증
         if(item.getPrice() != null && item.getQuantity() != null){
             int resultPrice = item.getPrice() * item.getQuantity();
             if (resultPrice < 10000){
-                errors.put("globalError", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice);
+                //field에러가 아닌 글로벌 에러를 처리하기위해 ObjectError 객체사용
+                bindingResult.addError(new ObjectError("item", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice));
             }
         }
 
         //검증에 실패하면 다시 입력폼으로
-        if (!errors.isEmpty()){ //errors맵이 빈값이 아니면 오류가 있다는 뜻이지.
-            log.info("errors={}", errors);
-            model.addAttribute("errors", errors);
+        if (bindingResult.hasErrors()){ //errors맵이 빈값이 아니면 오류가 있다는 뜻이지.
+            log.info("errors={}", bindingResult);
+            //bindingResult는 자동으로 view에 넘어감. 그래서 model.addAttribute 안해줘도됨
             return "validation/v2/addForm";
         }
 
