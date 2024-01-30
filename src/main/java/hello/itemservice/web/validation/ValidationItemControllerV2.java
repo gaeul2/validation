8f6626e869313c0ab615f9e976c4d8a9694f95ea
0540 <0540@coreit.co.kr>
2024-01-30 14:47:22 +0900

    오류코드와 메시지 처리1

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -86,7 +86,7 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV2(@ModelAttribute Item item,
                             BindingResult bindingResult, //순서가 중요 얘는 @ModelAttribute 다음에 와야함! 그래야 item의 에러를 담을 수 있음
                             RedirectAttributes redirectAttributes,
@@ -110,6 +110,47 @@ public class ValidationItemControllerV2 {
             if (resultPrice < 10000){
                 //field에러가 아닌 글로벌 에러를 처리하기위해 ObjectError 객체사용
                 bindingResult.addError(new ObjectError("item", null,null,"가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice));
+            }
+        }
+
+        //검증에 실패하면 다시 입력폼으로
+        if (bindingResult.hasErrors()){ //errors맵이 빈값이 아니면 오류가 있다는 뜻이지.
+            log.info("errors={}", bindingResult);
+            //bindingResult는 자동으로 view에 넘어감. 그래서 model.addAttribute 안해줘도됨
+            return "validation/v2/addForm";
+        }
+
+        //성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item,
+                            BindingResult bindingResult, //순서가 중요 얘는 @ModelAttribute 다음에 와야함! 그래야 item의 에러를 담을 수 있음
+                            RedirectAttributes redirectAttributes,
+                            Model model) {
+
+        //검증 로직
+        if(!StringUtils.hasText(item.getItemName())){ //item.ItemName에 글자가 없으면
+            //오브젝트명(@ModelAttribute에 담기는 이름을 넣어주면 됨, 필드명, 디폴트 메세지.
+            bindingResult.addError(new FieldError("item","itemName", item.getItemName(), false, new String[]{"required.item.itemName"}, null,null));
+        }
+        if(item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000){ //가격이 없거나 천원보다 작거나 백만원보다 크면
+            bindingResult.addError(new FieldError("item","price", item.getPrice(), false, new String[]{"range.item.price"}, new Object[]{1000, 1000000},null));
+        }
+        if(item.getQuantity() == null || item.getQuantity() >= 9999){
+            bindingResult.addError(new FieldError("item","quantity", item.getQuantity(), false, new String[]{"max.item.quantity="}, new Object[]{9999},null));
+        }
+
+        //특정 필드가 아닌 복합 룰 검증
+        if(item.getPrice() != null && item.getQuantity() != null){
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000){
+                //field에러가 아닌 글로벌 에러를 처리하기위해 ObjectError 객체사용
+                bindingResult.addError(new ObjectError("item", new String[]{"totalPriceMin"},new Object[]{10000, resultPrice},null));
             }
         }
 
